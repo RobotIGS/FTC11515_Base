2495282bb76ab8d61ebfb2b9796e1fbf1169ef43
RobotIGS Laptop <robot.igs@igs-goettingen.de>
2023-06-01 17:52:17 +0200

    added code structure

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/BaseAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/BaseAutonomous.java
@@ -1,2 +1,14 @@
-package org.firstinspires.ftc.teamcode.OpModes.Autonomous;public class BaseAutonomous {
+package org.firstinspires.ftc.teamcode.OpModes.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public abstract class BaseAutonomous extends LinearOpMode {
+    public abstract void initialize();
+    public abstract void run();
+
+    public void runOpMode() {
+        initialize();
+        waitForStart();
+        run();
+    }
 }
