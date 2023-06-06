e1d662097ea9a80bfd0a0669f4cd613cfc565913
RobotIGS Laptop <robot.igs@igs-goettingen.de>
2023-06-06 17:34:10 +0200

    update functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/Functions/FunctionSigmoid.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/Functions/FunctionSigmoid.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode.Tools.Functions;
 
-public class FunctionSigmoid extends FunctionLinear {
-}
+public class FunctionSigmoid extends FunctionBase {
+    private double r;
+    private double n;
+    private double a;
+    private double k;
+
+    /**
+     * f(x) = r - (e^(-(x-n)/a))^k
+     * @param a shift of turning point and increase of slope
+     * @param r shift on y-axis
+     * @param n shift on x-axis
+     * @param k change of slope and turning point stays the same
+     */
+    public FunctionSigmoid(double a, double r, double n, double k) {
+        this.r = r < 0 ? r = 1 : Math.abs(r);
+        this.n = n > 0 ? n = 0 : -Math.abs(n);
+        this.a = a == 0 ? 0.1 : a ;
+        this.k = k < 2 ? 2 : Math.abs(k);
+    }
+
+    public double apply(double x) {return r - Math.exp(Math.pow(-((x-n)/a),k));};
+
+    }
+
