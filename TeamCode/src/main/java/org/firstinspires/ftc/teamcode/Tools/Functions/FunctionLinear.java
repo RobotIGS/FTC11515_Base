1d361276b5f3a9457166e7f81d3e64ef15ed44ba
RobotIGS Laptop <robot.igs@igs-goettingen.de>
2023-06-06 17:34:10 +0200

    update functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/Functions/FunctionLinear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/Functions/FunctionLinear.java
@@ -1,10 +1,16 @@
 package org.firstinspires.ftc.teamcode.Tools.Functions;
 
-public class FunctionLiniear {
+public class FunctionLinear extends FunctionBase {
     private double a;
     private double b;
 
-    public FunctionLiniear(double a, double b) {
+    /**
+     * f(x) = a*x + b
+     * @param a slope of the function
+     * @param b starting value
+     */
+
+    public FunctionLinear(double a, double b) {
         this.a = a;
         this.b = b;
     }
