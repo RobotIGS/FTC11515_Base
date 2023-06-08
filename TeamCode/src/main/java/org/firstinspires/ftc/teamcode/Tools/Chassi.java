a36968aa818af0575eb706e30b0c1fedbbc77871
RobotIGS Laptop <robot.igs@igs-goettingen.de>
2023-06-08 17:53:26 +0200

    chassi update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/Chassi.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/Chassi.java
@@ -4,13 +4,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
 public interface Chassi {
-	private Position drivenDistance;
-	private double[] wheelSpeeds;
-	private DcMotor[] wheelMotors;
-
-	public void setVelocity(Velocity v);
-	protected void setMotorSpeeds(double[] v);
-	public void step();
-	public Velocity getDistance();
-	public void stopMotors();
+	void setVelocity(Velocity v);
+	void step();
+	Position getDistance();
+	void stopMotors();
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/ChassiBase.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.Tools;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+public abstract class ChassiBase implements Chassi {
+    private Position drivenDistance;
+    private double[] wheelSpeeds;
+    private DcMotor[] wheelMotors;
+
+    /**
+     * create chassi
+     */
+    public ChassiBase() {
+        drivenDistance = new Position();
+    }
+
+    /**
+     * set Motor powers
+     */
+    private void setMotorSpeeds() {
+        for (int i = 0; i < wheelMotors.length; i++) {
+            wheelMotors[i].setPower(wheelSpeeds[i]);
+        }
+    }
+
+    /**
+     * set wheel speeds based on velocity
+     * @param v robot velocity
+     */
+    @Override
+    public abstract void setVelocity(Velocity v);
+
+    /**
+     * get driven distance since last step
+     * @return driven distance
+     */
+    @Override
+    public Position getDistance() {
+        return drivenDistance;
+    }
+
+    /**
+     * stop chassi movement
+     */
+    @Override
+    public void stopMotors() {
+        setVelocity(new Velocity());
+        setMotorSpeeds();
+    }
+
+    /**
+     * update
+     */
+    @Override
+    public void step() {
+        setMotorSpeeds();
+    }
+
+}
