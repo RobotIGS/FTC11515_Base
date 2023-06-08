e7fa98c297cd2e2804b9e262d5c77e360b02555e
RobotIGS Laptop <robot.igs@igs-goettingen.de>
2023-06-08 16:53:24 +0200

    Update Profile and Funcions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/Profile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/Profile.java
@@ -1,39 +1,67 @@
 package org.firstinspires.ftc.teamcode.Tools;
 
-// TODO: replace t in step by internal variable saving the starttime (call of setDIrection)
+import org.firstinspires.ftc.teamcode.Tools.Functions.FunctionBase;
+
+// TODO: check if "System.currentTimeMillis" works
 
 public class Profile {
     private boolean acceleration;
-    private FunctionLinear accelerationFunction;
-    private FunctionLinear deccelerationFunction;
+    private FunctionBase accelerationFunction;
+    private FunctionBase decelerationFunction;
+    private long start_time;
 
-    /*
-     * Profile used to accelerate and deccelerate based on a function
+    /**
+     * Profile used to accelerate and decelerate based on a function
      * @param acceFunc function used for acceleration
-     * @param decceFunc function used for decceleration
+     * @param decelFunc function used for deceleration
      */
-    public Profile(FunctionLinear acceFunc, FunctionLinear deccelFunc) {
+    public Profile(FunctionBase acceFunc, FunctionBase decelFunc) {
         acceleration = true;
         accelerationFunction = acceFunc;
-        deccelerationFunction = deccelFunc;
+        decelerationFunction = decelFunc;
+        start_time = System.currentTimeMillis();
     }
 
-    /*
-     * set if acceleration or decceleration is used
+    /**
+     * set if acceleration or deceleration is used and reset start time
      * @param acce if acceleration is used
      */
     public void setDirection(boolean acce) {
         acceleration = acce;
+        resetStarTime();
     }
 
-    /*
-     * apply functions and return their values
-     * @param t time since start
+    /**
+     * set acceleration function
+     * @param acceFunction acceleration function
+     */
+    public void setAccelerationFunction(FunctionBase acceFunction) {
+        this.accelerationFunction = acceFunction;
+    }
+
+    /**
+     * set deceleration function
+     * @param decelFunction deceleration function
+     */
+    public void setDecelerationFunction(FunctionBase decelFunction) {
+        this.decelerationFunction = decelFunction;
+    }
+
+    /**
+     * reset start time
+     */
+    public void resetStarTime() {
+        start_time = System.currentTimeMillis();
+    }
+
+    /**
+     * apply functions on lap time in mills and return their values
      * @return some factor
      */
-    public double step(double t) {
+    public double step() {
+        long t = System.currentTimeMillis() - start_time;
         if (t >= 0)
             return accelerationFunction.apply(t);
-        return deccelerationFunction.apply(t);
+        return decelerationFunction.apply(t);
     }
 }
