327f5c5a7aeefe1f43d073f8817b2d21ef30db01
RobotIGS Laptop <robot.igs@igs-goettingen.de>
2023-06-13 17:56:42 +0200

    started working on FieldNavigation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/FieldNavigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/FieldNavigation.java
@@ -1,18 +1,38 @@
 package org.firstinspires.ftc.teamcode.Tools;
 
-import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import org.firstinspires.ftc.teamcode.Tools.DTypes.Position2D;
 
 import java.util.Date;
 
 public class FieldNavigation {
     private boolean driving;
-    private Position position;
+    private Position2D position;
     private double rotation;
-    private Position target_position;
+    private Position2D target_position;
     private double driving_accuracy;
     private Velocity velocity;
     private Date last_exec;
+
+    /**
+     * create new FieldNavigation object with given position and rotation
+     * @param position position of the robot in CM
+     * @param rotation rotation of the robot in Degrees
+     */
+    public FieldNavigation(Position2D position, double rotation) {
+        this.driving = false;
+        this.position = position;
+        this.rotation = rotation;
+        this.target_position = position;
+        this.driving_accuracy = 1;
+    }
+
+    /**
+     * create new FieldNavigation object with (0|0) as position and 0 as rotation
+     */
+    public FieldNavigation() {
+        this(new Position2D(), 0.0);
+    }
 
     /**
      * return if the robot is currently driving
@@ -34,7 +54,7 @@ public class FieldNavigation {
      * drive to position
      * @param p target position
      */
-    public void drive_pos(Position p) {
+    public void drive_pos(Position2D p) {
         driving = true;
         target_position = p;
     }
@@ -43,8 +63,9 @@ public class FieldNavigation {
      * drive a relative distance
      * @param d relative target position
      */
-    public void drive_rel(Position d) {
-        // TODO: calculate and set real target position
+    public void drive_rel(Position2D d) {
+        d.rotate(-this.rotation);
+        d.add(this.position);
         drive_pos(d);
     }
 
@@ -60,7 +81,7 @@ public class FieldNavigation {
      * get current position
      * @return current position
      */
-    public Position getPosition(){
+    public Position2D getPosition(){
         return position;
     }
 
@@ -76,7 +97,7 @@ public class FieldNavigation {
      * set current position
      * @param p position
      */
-    public void setPosition(Position p) {
+    public void setPosition(Position2D p) {
         position = p;
     }
 
@@ -84,10 +105,9 @@ public class FieldNavigation {
      * calculate current position utilising the driven distance since the last refresh
      * @param d the driven distance
      */
-    public void calculatePosition(Position d) {
-        // TODO: replace with function
-        position.x += d.x;
-        position.y += d.y;
+    public void calculatePosition(Position2D d) {
+        // TODO: try replace with operator
+        position.add(d);
     }
 
 
@@ -95,7 +115,21 @@ public class FieldNavigation {
      * refresh
      */
     public void step() {
-        // TODO: calculate and set new target velocity
+        Position2D distance;
+
+        distance = target_position.copy();
+        distance.subract(position);
+
+        if (Math.abs(distance.getAbsolute()) <= this.driving_accuracy) {
+            driving = false;
+            velocity = new Velocity();
+        }
+
+        if (driving) {
+            distance = distance.getNormalization();
+            velocity.xVeloc = distance.getX();
+            velocity.yVeloc = distance.getY();
+        }
         // TODO: unset driving if target reached (with driving_accuracy)
     }
 }
