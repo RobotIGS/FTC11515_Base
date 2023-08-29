a3c7eaf4aabc83c9bde104f1630cbfd3e3efcbfa
RobotIGS Laptop <robot.igs@igs-goettingen.de>
2023-08-29 17:46:03 +0200

    upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/Chassis/MecanumChassi.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/Chassis/MecanumChassi.java
@@ -15,23 +15,33 @@ import org.firstinspires.ftc.teamcode.Tools.DTypes.Position2D;
  *
  */
 
-//TODO: all ig
 public class MecanumChassi extends ChassiBase {
-    private final double ONE_OVER_R = 1; //TODO check + add variable(r) + const
-    private final double R_OVER_4 = 1;
-    private final int l = 1;
+    private final double WHEELDIAMETER = 10;
+    private final double ONE_OVER_R = 1/(WHEELDIAMETER/2); //TODO check + add variable(r) + const
+    private final double R_OVER_4 = (WHEELDIAMETER/2)/4;
+    private final int l = 1; // (a+b); // TODO a + b
+
     public MecanumChassi() {
         super(4);
     }
 
     @Override
     public void setVelocity(Velocity velocity) {
+        double vm;
         super.setVelocity(velocity);
         // TODO: add source (vy inverted + right side inverted)
         wheelSpeeds[0] =  (velocity.getVX() + velocity.getVY() - l * velocity.getWZ()) * ONE_OVER_R;
         wheelSpeeds[1] = -(velocity.getVX() - velocity.getVY() + l * velocity.getWZ()) * ONE_OVER_R; // negate because right
         wheelSpeeds[2] = -(velocity.getVX() + velocity.getVY() + l * velocity.getWZ()) * ONE_OVER_R; // negate because right
         wheelSpeeds[3] =  (velocity.getVX() - velocity.getVY() - l * velocity.getWZ()) * ONE_OVER_R;
+
+        // normalize the values
+        vm = Math.max(Math.max(wheelSpeeds[0], wheelSpeeds[1]), Math.max(wheelSpeeds[2], wheelSpeeds[3]));
+        wheelSpeeds[0] /= vm;// .. / max_speed;
+        wheelSpeeds[1] /= vm;
+        wheelSpeeds[2] /= vm;
+        wheelSpeeds[3] /= vm;
+
     }
 
     @Override
