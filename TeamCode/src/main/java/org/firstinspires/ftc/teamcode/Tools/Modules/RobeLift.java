98d8721c3eb994d0b59cf02f3dd0e7fe3343cf26
RobotIGS Laptop <robot.igs@igs-goettingen.de>
2023-09-28 15:37:31 +0200

    add RobeLift class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/Modules/RobeLift.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.Tools.Modules;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class RobeLift {
+    private DcMotor motor;
+    private float steps_min;
+    private float steps_max;
+
+    public void setPower(float power) {
+    }
+
+    public void setTargetPosition(float steps) {
+    }
+    public void step() {
+    }
+}
